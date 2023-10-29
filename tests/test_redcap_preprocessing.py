d1e11cf10c13338898e557e7881ad8c0cb8c05a5
Gustave Ronteix <gustave.ronteix@hotmail.fr>
2023-10-29 20:42:20 +0100

    start working on molecular data

--- tests/test_redcap_preprocessing.py
+++ tests/test_redcap_preprocessing.py
@@ -1,11 +1,2 @@
 import redcap_preprocessing.clinical_data_processor as cdp
-
-def test_functionality():
-    # Your test cases go here
-    result = module_file.some_function()
-    assert result == expected_result
-
-def test_edge_cases():
-    # More test cases
-    result = module_file.another_function()
-    assert result == expected_result
+import unittest
